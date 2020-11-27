122c1a97a2f882d586d537c0b4978613b1314f1b
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-27 16:29:09 -0700

    final commit

--- app.js
+++ app.js
@@ -32,6 +32,42 @@ app.get('/api/v1/footnotes', (request, response) => {
   });
 });
 
+app.post('/api/v1/papers', (request, response) => {
+  const paper = request.body;
+
+  for (let requiredParameter of ['title', 'author']) {
+    if (!paper[requiredParameter]) {
+      return response
+      .status(422)
+      .send({ error: `Expected format: { title: <String>, author: <String> }. You are missing "${requiredParameter}" property.` });
+    }
+  }
+
+  database('papers').insert(paper, 'id')
+    .then(paper => {
+      response.status(201).json({ id: paper[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+  });
+
+app.get('/api/v1/papers/:id', (request, response) => {
+  database('papers').where('id, request.params.id').select()
+    .then(papers => {
+      if (papers.length) {
+        response.status(200).json(papers);
+      } else {
+        response.status(404).json({
+          error: `Could not find paper with id ${request.params.id}`
+        });
+      }
+  })
+  .catch(error => {
+    response.status(500).json({ error });
+  });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
